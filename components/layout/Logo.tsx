3e1b04428ef3e42f2e07dc1c02769643d173bebc
Christina Mannerberg <chrasse@hotmail.com>
2023-12-06 20:16:21 +0100

    corrected spelling

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -6,7 +6,7 @@ const logoFont = Abril_Fatface({ weight: '400', subsets: ['latin'] });
 function Logo() {
   return (
     <Link href='/' className={`text-2xl lg:text-4xl ${logoFont.className}`}>
-      <span className='hidden md:inline'>whishlist.</span>
+      <span className='hidden md:inline'>wishlist.</span>
       <span className='text-3xl md:hidden'>w.</span>
     </Link>
   );
